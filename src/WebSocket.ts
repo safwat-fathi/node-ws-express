92dbc81cdd9077c81c3fbc696c5266e7aa5e7083
Safwat Fathi <safwat.rashwan@gmail.com>
2023-01-20 14:49:42 +0200

    add: singleton & todo from chatGPT

--- src/WebSocket.ts
+++ src/WebSocket.ts
@@ -4,51 +4,17 @@
 	- send method
 	- broadcast method
  */
-import { _WebSocket } from "./types/ws";
-import {
-  EventListenerOptions,
-  RawData,
-  Data,
-  WebSocket,
-  Server as WSS,
-} from "ws";
-import {
-  Server as HttpServer,
-  IncomingMessage,
-  IncomingHttpHeaders,
-} from "http";
+// import { WebSocket } from "./types/ws";
+import { RawData, WebSocket, Server as WSS } from "ws";
+import { Server as HttpServer, IncomingMessage } from "http";
 // import uuid from 'uuid'
 
 type WebSocketClients = Set<WebSocket>;
-// type HttpServer = Server<typeof IncomingMessage, typeof ServerResponse>;
-// type WSServer = WebSocketServer<_WebSocket>;
 
-export class WebSocketServer {
-  // public clients: WebSocketClients;
-  // public wsServer: WSServer;
-  // // private _server: HttpServer
-
-  // constructor(server: HttpServer) {
-  //   // this._server = server
-  //   this.wsServer = new _WebSocket.Server({ server });
-  //   this.clients = this.wsServer.clients;
-  // }
-
-  // init() {
-  //   this.wsServer.emit("connection");
-  // }
-
-  // event(
-  //   evType: string,
-  //   cb?: (ws: _WebSocket, request: IncomingMessage) => void
-  // ) {
-  //   // this.wsServer.on("close", )
-  //   this.wsServer.on("connection", (socket, req) => {
-  //     socket.send("Hi there, I am a _WebSocket server");
-  //   });
-  // }
+export default class WebSocketServer {
+  private static _instance: WebSocketServer | null = null;
   private readonly _wss: WSS;
-  private readonly _clients: WebSocketClients;
+  private _clients: WebSocketClients;
 
   constructor(httpServer: HttpServer) {
     this._wss = new WebSocket.Server({ server: httpServer });
@@ -57,35 +23,45 @@ export class WebSocketServer {
     this._attachEventListeners();
   }
 
-  listen(
-    event: "connection" | "error" | "close",
-    cb: (
-      _WebSocket: WSS,
-      socket: _WebSocket,
-      req: IncomingMessage,
-      headers: IncomingHttpHeaders
-    ) => void
-  ) {
-    // this._wss.(event, cb);
+  public static init(httpServer: HttpServer) {
+    if (this._instance === null) {
+      this._instance = new WebSocketServer(httpServer);
+    }
+
+    return this._instance;
   }
 
-  init(cb: () => void) {
-    this._wss.once("connection", cb);
-  }
+  // listen(
+  //   event: "connection" | "error" | "close",
+  //   cb: (
+  //     WebSocket: WSS,
+  //     socket: WebSocket,
+  //     req: IncomingMessage,
+  //     headers: IncomingHttpHeaders
+  //   ) => void
+  // ) {
+  //   // this._wss.(event, cb);
+  // }
+
+  // init(cb: () => void) {
+  //   this.wss.once("connection", cb);
+  // }
 
   send(
-    payload: { id: number; message: string },
-    socket: _WebSocket,
-    req: IncomingMessage
+    payload: { id: string; message: string },
+    socket: WebSocket
+    // req: IncomingMessage
   ) {
-    let senderId = this._clients.forEach(client => {
-      if (client === socket) {
-        client.send(`Message from: ${payload.id}  -> ${payload.message}`);
+    let senderId = payload.id;
+
+    this._clients.forEach(client => {
+      if (client !== socket) {
+        client.send(`Message from: ${senderId} -> ${payload.message}`);
       }
     });
   }
 
-  broadcast(payload: string, socket: _WebSocket) {
+  broadcast(payload: string, socket: WebSocket) {
     this._clients.forEach(client => {
       if (client !== socket && client["readyState"] === WebSocket["OPEN"]) {
         client.send(`Hello, broadcast message -> ${payload}`);
@@ -94,18 +70,19 @@ export class WebSocketServer {
   }
 
   private _attachEventListeners(): void {
-    this._wss.on("connection", (socket: _WebSocket, req: IncomingMessage) => {
+    this._wss.on("connection", (socket: WebSocket, req: IncomingMessage) => {
       let clientId = req.headers["sec-websocket-key"];
-      socket.id = <string>clientId;
+      // this._clients = this._wss.clients;
+      this._clients.add(socket);
 
       console.log(`client with id: ${clientId} connected`);
 
       // When a message is received from the client
       socket.on("message", (data: RawData) => {
-        console.log(`Message received from client : ${data}`);
+        console.log(`Message: "${data}" received from client`);
 
         // Send back a response to the client with some data
-        socket.send(JSON.stringify({ sender: socket.id }));
+        // console.log(this._clients);
       });
 
       // When the connection is closed
@@ -114,8 +91,8 @@ export class WebSocketServer {
       });
     });
 
-    // this._wss.on("close", )
+    this._wss.on("close", () => {
+      console.log("Server closed");
+    });
   }
-
-  // private receive(msg: Data, cb) {}
 }
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,13 @@
 import express from "express";
 import http from "http";
-import { WebSocketServer } from "./WebSocket";
+import WebSocketServer from "./WebSocket";
 
 const app = express();
 
 const server = http.createServer(app);
-const wss = new WebSocketServer(server);
+WebSocketServer.init(server);
 
 // start our server
 server.listen(process.env.PORT || 9000, () => {
   console.log(`Server started`);
-});
-
-// listen to WebSocket server when a new connection is established
-wss.init(() => {
-  console.log("New WebSocket connection established");
 });
